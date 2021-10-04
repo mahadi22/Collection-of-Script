f4fa3e7aaed19e841643391af4eb2056549bc3c6
evansusu <57264858+evansusu@users.noreply.github.com>
2021-10-04 15:40:04 +0800

    add lcm python

---
+++ scripts/lcm.py
@@ -0,0 +1,15 @@
+def compute_lcm(x, y):
+
+    # choose the greater number
+    if x > y:
+        greater = x
+    else:
+        greater = y
+
+    while(True):
+        if((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
